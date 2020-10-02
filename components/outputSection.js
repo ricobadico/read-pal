fe8c282eacf5d856e7f7d22cbba4792928d20978
Eric Biondic <biondiceric@gmail.com>
2020-10-02 13:09:20 -0600

    lang test4

--- components/outputSection.js
+++ components/outputSection.js
@@ -5,7 +5,7 @@ export default function OutputSection( { sentenceArray, getVoice }) {
 
     const speakSentence = (text) => {
         if (typeof window !== "undefined") {
-                var msg = new SpeechSynthesisUtterance();
+                let msg = new SpeechSynthesisUtterance();
                 var voices = (window.speechSynthesis.getVoices());
                 if(window.speechSynthesis.onvoiceschanged !== undefined){
                     window.speechSynthesis.onvoiceschanged = window.speechSynthesis.getVoices()
@@ -17,7 +17,7 @@ export default function OutputSection( { sentenceArray, getVoice }) {
                 msg.rate = 1; // From 0.1 to 10
                 msg.pitch = 2; // From 0 to 2
                 msg.text = text;
-                msg.lang = 'en';
+                msg.lang = 'en-us';
                 speechSynthesis.cancel();
                 speechSynthesis.speak(msg);
                 }
