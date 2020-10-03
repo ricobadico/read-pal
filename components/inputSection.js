fb0c9d3e87dde767281d653160c80b7bd5ec148a
Eric Biondic <biondiceric@gmail.com>
2020-10-03 08:25:17 -0600

    fixing autofocus ugliness on ios

--- components/inputSection.js
+++ components/inputSection.js
@@ -2,13 +2,21 @@ import styles from '../styles/inputSection.module.css'
 import Link from 'next/link'
 
 export default function InputSection( { inputText, onInputChange, onInputSubmit }) {
+
+    const selectText = (event) => {
+        let textElement = event.currentTarget;
+        textElement.select();
+    }
+
      return (
         <div className={styles.grid}>
                 <textarea rows="20" cols="150"
                 className={styles.textInputBox} name="inputText"
-                autoFocus={true}
+                autoFocus={false}
+                onClick={(event => selectText(event))}
                 defaultValue={inputText}
                 onChange={onInputChange}
+                id="inputTextArea"
                 />
                 <Link 
                 href={{
