d411c1475937b0a2ac56c093b3f4cc0b36665958
Eric Biondic <biondiceric@gmail.com>
2020-10-03 09:37:17 -0600

    making inputbox nicer on IOS

--- components/inputSection.js
+++ components/inputSection.js
@@ -3,18 +3,12 @@ import Link from 'next/link'
 
 export default function InputSection( { inputText, onInputChange, onInputSubmit }) {
 
-    const selectText = (event) => {
-        let textElement = event.currentTarget;
-        textElement.select();
-    }
-
      return (
         <div className={styles.grid}>
                 <textarea rows="20" cols="150"
                 className={styles.textInputBox} name="inputText"
                 autoFocus={false}
-                onClick={(event => selectText(event))}
-                defaultValue={inputText}
+                placeholder={inputText}
                 onChange={onInputChange}
                 id="inputTextArea"
                 />
