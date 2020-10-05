49d17b4fb6186e2df4cce53776425c1ff620153a
Eric Biondic <biondiceric@gmail.com>
2020-10-05 06:51:43 -0600

    tidying some spacing

--- pages/_app.js
+++ pages/_app.js
@@ -4,14 +4,13 @@ import App, { Container } from 'next/app'
 export default class MyApp extends App {
 
   state={
-    inputText: "Put some text here!",
-    test: "seadog"
+    // This gets updated and needs to be tracked across pages
+    inputText: "Paste some text here!",
   }
 
    // Captures text placed in the inputSection
   onInputChange = (event) => {
     this.setState({inputText: event.target.value});
-    console.log(event.target.value);
   }
 
   render(){
